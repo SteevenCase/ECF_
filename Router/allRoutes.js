9af4d4233b0f13e77439c2998bb45ead0635e632
SteevenG <roxas76500@live.fr>
2025-05-27 19:15:46 +0200

    responsivité acceuil et amelioration

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -1,7 +1,12 @@
 import Route from "./Route.js";
 
 //Définir ici vos routes
-export const allRoutes = [new Route("/", "Accueil", "/pages/home.html")];
+export const allRoutes = [
+  new Route("/", "Accueil", "/pages/home.html"),
+  new Route("/connexion", "connexion", "/pages/connexion.html"),
+  new Route("/inscription", "inscription", "/pages/inscription.html"),
+  new Route("/covoiturages", "covoiturages", "/pages/covoiturages.html"),
+];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
-export const websiteName = "Quai Antique";
+export const websiteName = "EcoRide";
